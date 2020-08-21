62ecd08def0777fdda43320c40c826d6923db628
Jaime Zendejas <j.zendejas@berkeley.edu>
2020-08-20 19:08:08 -0700

    built file browser

---
+++ first_draft.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+
+class Root(Tk):
+	def __init__(self):
+		super(Root, self).__init__()
+		self.title('Game Display')
+		self.minsize(640, 400)
+		self.wm_iconbitmap('calc_img.ico')
+		self.labelFrame = ttk.LabelFrame(self, text = "Open File")
+		self.labelFrame.grid(column=0, row=1, padx=20, pady=20)
+		self.button()
+		
+	def button(self):
+		self.button = ttk.Button(self.labelFrame, text="Browse A File", command= self.fileDialog)
+		self.button.grid(column=1, row=1)
+		
+	def fileDialog(self):
+		self.filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetype=(("jpeg files", "*jpg"), ("all files", "*.*")))
+		self.label = ttk.Label(self.labelFrame, text="")
+		self.label.grid(column=1, row=2)
+		self.label.configure(text=self.filename)
